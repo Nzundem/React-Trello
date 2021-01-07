3acb08cf23241be616e93662b75a0aa278f351c9
Nzoundem Essien <nzundemessien18@gmail.com>
2021-01-07 08:34:34 +0100

    added form for adding new card and list

--- src/components/List.js
+++ src/components/List.js
@@ -2,6 +2,22 @@ import React from 'react'
 import Card from './Card'
 import PropTypes from 'prop-types'
 class List extends React.Component {
+    state ={
+        currentCards: []
+    }
+    nameInput = React.createRef()
+    createNewCard = (e) =>{
+        e.preventDefault()
+        const card = {
+            id: Math.ceil(Math.random() * 1000),
+            text: this.nameInput.current.value
+        }
+        if(card.text){
+            this.setState({currentCards: [...this.state.currentCards, card]})
+        }
+        console.log(this.nameInput.current.value)
+        // this.nameInput.current.value = ''
+    }
     render(){
         return (
             <div className="list">
@@ -12,6 +28,12 @@ class List extends React.Component {
                 data={this.props.list.cards[key]}
                 />
             ))}
+
+            <form onSubmit={this.createNewCard}
+            className="new-card-wrapper">
+            <input type="text" name="name" ref={this.nameInput} placeholder="+ New card"/>
+
+            </form>
             </div>
         )
     }
